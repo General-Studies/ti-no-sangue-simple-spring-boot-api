3d6dc703ccd6e227dbdcf35dd3216185def937cd
Jether Rodrigues <jether.rodrigues@geovoxel.com>
2020-06-21 02:01:44 -0300

    start api to TI no Sangue series

---
+++ src/main/java/com/jetherrodrigues/apitinosangue/ApiTiNoSangueApplication.java
@@ -0,0 +1,13 @@
+package com.jetherrodrigues.apitinosangue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTiNoSangueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTiNoSangueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jetherrodrigues/apitinosangue/ApiTiNoSangueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetherrodrigues.apitinosangue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTiNoSangueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
